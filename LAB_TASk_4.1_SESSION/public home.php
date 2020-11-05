d4f5eb3262ab60adb2a5e000a9453153a6cd1049
Aklima <you@example.com>
2020-11-05 10:33:34 +0600

    lab4

---
+++ LAB_TASk_4.1_SESSION/public home.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+    <title>X Company Home</title>
+</head>
+<body>
+    <form>
+        <table border ="1" width="50%" align="center">
+            <tr>
+                <td><img width="50" height="35" src="images/logo.png"></td>
+            </tr>
+            <tr>
+                <td colspan="4"><h1>Welcome to xCompany</h1></td>
+            </tr>
+            <tr>
+                <td align="center" colspan="4">Copyright 2017</td>
+            </tr>
+        </table>
+    </form>
+</body>
+</html>
