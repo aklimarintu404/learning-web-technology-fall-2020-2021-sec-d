d8b25bc6fffb5e9992e5cca16695ea4461f7ada7
Aklima <you@example.com>
2020-11-06 17:59:28 +0600

    lab3.2

--- LAB_TASK_3.2_PHP/nameCheck.php
+++ LAB_TASK_3.2_PHP/nameCheck.php
@@ -1,27 +1,17 @@
-
 <?php
-if(isset($_GET['msg'])){
-	echo "invalid request";
-}
 
-?>
-
-
-<html>
-<head>
-	<title>NAME</title>
-</head>
-<body>
-	<tr>
-<form method= "post" action="name.php">
-		<th align="left"><h4>1.</h4></th>
-		<fieldset style="width:180px;">
-		
-		<th>Name</th><br>
-		<input type="text" name="name"  > <br><br>
-		<input type="submit" value="submit">
-		</fieldset>
-</form> 
-	</tr>
-</body>
-</html>
+if(isset($_REQUEST['submit'])){
+    $name = $_REQUEST['name'];
+    if(!empty($name)){
+         if($name[0]==" "){
+             header('location: name.html?msg=invalid_name');
+         }
+         
+    else{    
+         header('location: name.html?msg=null_name');       
+     }   
+ }
+ else{   
+     header('location: name.html?msg=error');   
+ }
+ ?>
