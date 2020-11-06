98cc1839ed8d530fb518f5d233c321576e7698ee
Aklima <you@example.com>
2020-11-06 23:44:38 +0600

    lab4.1

---
+++ LAB_TASk_4.1_SESSION/forgot password.php
@@ -0,0 +1,32 @@
+<html>
+<head>
+    <title>X Company</title>
+</head>
+<body>
+    <form>
+        <table border ="1" width="100%"  >
+            <tr>
+                <td width="100px" ><img width="100px" height="100px" src="company.jpg"></td>
+		<td colspan="2" align="right">
+                <a href="public_home.html">Home</a> |
+                <a href="login.html">Login</a> |
+                <a href="registration.html">Registration</a></td>
+            </tr>
+			
+            <tr height="400px" >
+			 <td colspan="2"" 
+			<td   
+			 <form method= "post" action="" >
+			 <fieldset >
+			 <legend><b>FORGOT PASSWORD</b></legend>
+			 <pre>Enter Email : <input type="text" name="name"></pre><hr>
+			 <input type="submit" value="Submit">
+			  	
+            </tr>
+            <tr>
+                <td align="center" colspan="4">Copyright &#169; 2017</td>
+            </tr>
+        </table>
+    </form>
+</body>
+</html>
---
+++ LAB_TASk_4.1_SESSION/registration.php
@@ -0,0 +1,53 @@
+<html>
+<head>
+    <title>X Company</title>
+</head>
+<body>
+    <form>
+        <table border ="1" width="100%" colspan="2" >
+            <tr>
+                <td width="100px" height="100px" ><img width="100px" height="100px" src="company.jpg"></td>
+		<td colspan="2" align="right">
+                <a href="public_home.html">Home</a> |
+                <a href="login.html">Login</a> |
+                <a href="registration.html">Registration</a></td>
+            </tr>
+			<tr height="400px" >
+			 <td colspan="2"
+		<td>
+		<form method= "post" action="" >
+		
+		<fieldset >
+            
+		<legend><b>REGISTRATION</b></legend>
+		<pre>Name            : <input type="text" name="name"></pre><hr>
+        <pre>Email           : <input type="email" name="email">
+        <button title="hint:Sample@example.com"><b>i</b></button></pre><hr>
+		<pre>User Name       : <input type="text" name="user name"></pre><hr>
+		<pre>Confirm Password: <input type="text" name="confirm password"><hr></pre>
+		<fieldset style="width:200px">
+		
+		<legend><b>Gender</b></legend>
+		<input type="radio" name="gender"  > Male 
+		<input type="radio" name="gender"  > Female
+		<input type="radio" name="gender"  > Other <br>
+        </fieldset><hr>
+		
+		<fieldset style="width:200px">
+		<legend><b>Date of Birth</b></legend>
+		<input type="tel" size="1"> <b> /</b> <input type="tel" size="1"> <b> /</b> <input type="tel" size="2"> (dd/mm/yyyy)
+        </fieldset><hr>
+		
+		<input type="submit" value="Submit">
+		<input type="reset" name="" value="Reset"> 
+        </fieldset >
+		</td>
+		</td>	
+			</tr>
+            <tr>
+                <td align="center" colspan="4">Copyright &#169; 2017</td>
+            </tr>
+        </table>
+    </form>
+</body>
+</html>
