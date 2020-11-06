c5ce3765d40813a75bb8ceb7dbaf5280fe0c82e2
Aklima <you@example.com>
2020-11-06 19:12:34 +0600

    lab4.1

--- LAB_TASk_4.1_SESSION/public home.php
+++ LAB_TASk_4.1_SESSION/public home.php
@@ -4,19 +4,19 @@
 </head>
 <body>
     <form>
-        <table border ="1" width="900px" align="center">
+        <table border ="1" width="100%"  >
             <tr>
-                <td><img width="50" height="35" src="images/logo.png"></td>
+                <td width="100px" ><img width="100px" height="100px" src="company.jpg"></td>
 		<td colspan="2" align="right">
-                <a href="public_home.html">Home</a>
-                <a href="login.html">Login</a>
-                <a href="registration.html">Register</a></td>
+                <a href="public_home.html">Home</a> |
+                <a href="login.html">Login</a> |
+                <a href="registration.html">Registration</a></td>
+            </tr>
+            <tr height="400px" >
+                <td colspan="4"><h3>Welcome to xCompany</h3></td>
             </tr>
             <tr>
-                <td colspan="4"><h1>Welcome to xCompany</h1></td>
-            </tr>
-            <tr>
-                <td align="center" colspan="4">Copyright 2017</td>
+                <td align="center" colspan="4">Copyright &#169; 2017</td>
             </tr>
         </table>
     </form>
