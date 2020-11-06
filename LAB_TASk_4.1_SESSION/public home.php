da947aa6be4067cec578752848b04f2dcfe2fd9f
Aklima <you@example.com>
2020-11-06 21:12:38 +0600

    lab4.1

---
+++ LAB_TASk_4.1_SESSION/public home.php
@@ -0,0 +1,24 @@
+<html>
+<head>
+    <title>X Company</title>
+</head>
+<body>
+    <form>
+        <table border ="1" width="100%"  >
+            <tr>
+                <td width="100px" ><img width="100px" height="100px" src="company.jpg"></td>
+		<td colspan="2" align="right">
+                <a href="public_home.html">Home</a> |
+                <a href="login.html">Login</a> |
+                <a href="registration.html">Registration</a></td>
+            </tr>
+            <tr height="400px" >
+                <td colspan="4"><h3>Welcome to xCompany</h3></td>
+            </tr>
+            <tr>
+                <td align="center" colspan="4">Copyright &#169; 2017</td>
+            </tr>
+        </table>
+    </form>
+</body>
+</html>
