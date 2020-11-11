2755af78883208c1715468a5c06ffb20e4377f01
Aklima <you@example.com>
2020-11-11 09:03:25 +0600

    mid

---
+++ MID_LAB_EXAM/login.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+    <title>HOME</title>
+</head>
+<body>
+    
+			 <form method= "post" action="" >
+			 <fieldset >
+			 <legend><b>LOGIN</b></legend>
+			 <pre>User Name : <input type="text" name="name"></pre>
+			 <pre>Password  : <input type="text" name="password"></pre><hr>
+			 <input type="submit" name="login" value="login">
+			 <a href="register.html">Register</a>
+			 </fieldset >
+			 </form>
+			 </body>
+			 </html>
