a765ff1ad9c170e6cbd7eccaf81544d27c41a437
Aklima <you@example.com>
2020-11-11 08:50:07 +0600

    mid

---
+++ MID_LAB_EXAM/registration.php
@@ -0,0 +1,25 @@
+
+
+
+<html>
+<head>
+    <title>HOME</title>
+</head>
+<body>
+ <form method= "post" action="" >
+			 <fieldset >
+			 <legend><b>REGISTRATION</b></legend>
+			 <pre>ID                : <input type="id" name="id"></pre>
+			 <pre>Password          : <input type="text" name="password"></pre>
+			 <pre>Confirm Password  : <input type="text" name="password"></pre>
+			 <pre>Name              : <input type="name" name="name"></pre>
+			 User Type<hr>
+			 User<input type="radio" name="user" value="user"> admin <input type="radio" name="admin" value="admin"> <hr>
+			 <input type="submit" name="sign up" value="sign up">
+			 <a href="login.html">Sign In</a>
+			 </fieldset >
+			 </form>
+			 </body>
+			 </html>
+			 
+			 
