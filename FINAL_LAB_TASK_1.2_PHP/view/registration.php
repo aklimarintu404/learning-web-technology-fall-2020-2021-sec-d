e7134e7c85c5cef5b63e0144dd3a4724c43b64e6
Aklima <you@example.com>
2020-12-02 08:44:54 +0600

    lab1.2

---
+++ FINAL_LAB_TASK_1.2_PHP/view/registration.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html >
+<head>
+	<title>REGISTRATION</title>
+</head>
+<body>
+	<fieldset>
+		<legend><b>REGISTRATION</b></legend>
+		<form action="registration.php" method="POST">
+			<br/>
+			<table width="100%"  cellspacing="0">
+			Name : <input type="text" name="name"><br><br>
+			UserName : <input type="text" name="username"><br><br>
+            Contact No : <input type="text" name="contactNo"><br><br>
+            password : <input type="password" name="password"><br><br>
+            <input type="submit" name="submit" value="Register">
+			</table>
+			<hr/>
+			<input type="submit" name= "submit" value="Submit">
+			<input type="reset">
+		</form>
+	</fieldset>
+</body>
+</html>
