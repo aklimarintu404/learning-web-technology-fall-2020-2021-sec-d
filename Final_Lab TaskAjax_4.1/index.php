0491f6acbf01363c6b74270b0799109683e2755e
Aklima <you@example.com>
2020-12-14 09:40:32 +0600

    final

---
+++ Final_Lab TaskAjax_4.1/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login</title>
+</head>
+<body>
+    
+            
+            <fieldset >
+            
+            <legend>Login</legend>
+            
+            <form>
+                
+                User Id <br>
+                <input type="text" name="userId"><br>
+                Password <br>
+                <input type="text" name="password"><br>
+                <input type="checkbox" name="rememberMe" value="Remember Me"> Remember Me
+                <hr>
+                <input type="submit" name="submit" value="Login"> <a href="registration/registration.php">Register</a>
+                
+            </form>
+            
+        </fieldset>
+            
+            
+        
+        
+    </center>
+    
+</body>
+</html>
